a73d2a6e6b321b5004c620d6fccf2003ffc73a89
Earl Kian A. Bancayrin <earlkian8@gmail.com>
2025-11-22 03:06:59 +0800

    fix: dashboard route on authentication

--- backend/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ backend/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -39,7 +39,7 @@ class AuthenticatedSessionController extends Controller
                 ->with('success', 'Login Successfully! Please verify your email.');
         }
 
-        return redirect()->intended(route('dashboard'))
+        return redirect()->intended(route('admin.dashboard'))
             ->with('success', 'Login Successfully!');
     }
 
